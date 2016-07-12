02f852e2fe67449c2182d96c0dbe57e57bc045cc
Jose Constela <jose@joseconstela.com>
2016-07-12 18:45:46 +0200

    add code mirror support

--- client/modules/jobs/index.js
+++ client/modules/jobs/index.js
@@ -1,6 +1,24 @@
 Template.jobsTableItem.events({
   "click .job-status-switch": (event, template) => {
     let newStatus = $(event.target).data('job-status')
-    Meteor.call('updateJobStatus', template.data.jobId, newStatus);
+    Meteor.call('updateJobStatus', template.data._id, newStatus);
   }
 })
+
+Template.jobsAdd.rendered = function() {
+  if ($('textarea').length)
+  var editor = CodeMirror.fromTextArea($('textarea')[0], {
+    lineNumbers: true,
+    styleActiveLine: true,
+    matchBrackets: true
+  });
+}
+
+Template.jobsEdit.rendered = function() {
+  if ($('textarea').length)
+  var editor = CodeMirror.fromTextArea($('textarea')[0], {
+    lineNumbers: true,
+    styleActiveLine: true,
+    matchBrackets: true
+  });
+}
