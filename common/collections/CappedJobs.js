02421c87e8ffa52e481ff633c8b904285a4016ed
Jose Constela <jose@joseconstela.com>
2016-07-17 14:43:02 +0200

    fix permissions for collections

--- common/collections/CappedJobs.js
+++ common/collections/CappedJobs.js
@@ -2,6 +2,6 @@ CappedJobs = new Meteor.Collection('CappedJobs');
 
 CappedJobs.allow({
   insert: function () { return !!Meteor.user(); },
-  update: function () { return !!Meteor.user(); },
-  remove: function () { return !!Meteor.user(); }
+  update: function () { return false; },
+  remove: function () { return false; }
 });
