871375a7ab2687f4df802b8fa773cc3b131a4f2c
Jose Constela <jose@joseconstela.com>
2016-07-21 23:27:45 +0200

    Change demo mode login credentials

--- client/modules/site/js.js
+++ client/modules/site/js.js
@@ -1,5 +1,5 @@
 Template.site.events({
   "click .membership-demo-login": function(event, template){
-    Meteor.loginWithPassword('admin@admin.com', 'admin');
+    Meteor.loginWithPassword('demo@demo.com', 'demo');
   }
 });
