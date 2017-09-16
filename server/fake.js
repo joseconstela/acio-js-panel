45fcbb2a2912326092686dd72256533abb45504e
Jose Constela <jose@joseconstela.com>
2017-09-16 18:57:40 +0200

    Fake correct data

--- server/fake.js
+++ server/fake.js
@@ -1,37 +1,76 @@
 Meteor.startup(() => {
 
-  if(process.env.NODE_ENV === 'development' || !!Meteor.settings.public.demoMode) {
-    /*Jobs.remove({})
+  if (process.env.NODE_ENV === 'development' ||  !!Meteor.settings.public.demoMode) {
+    Templates.remove({})
+    Jobs.remove({})
+    Collections.remove({})
     JobsResults.remove({})
     Meteor.users.remove({})
 
-    console.log('fake');
-
     let userId = Accounts.createUser({
-      email : 'demo@demo.com',
-      password : 'demo'
+      email: 'demo@demo.com',
+      password: 'demo'
     })
 
-    for(var i = 0;i<=9;i++)
-    Jobs.insert({
-      name: "default_job_00"+i+"-RANDOM",
-      description: "Test created by server/fake.js",
-      status: "stopped",
-      code: `setInterval(function() {result(1, {reqNewJob:false} );}, 1000)`,
-      history: [
-        {
-          status: 'stopped',
+    let tplId = Templates.insert({
+      name: 'My template',
+      libraries: [
+        'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.2/lodash.min.js'
+      ],
+      code: 'setInterval(function() {result(\'lodash\', {reqNewJob:false} );}, 300)',
+      createdAt: new Date()
+    })
+
+    let collectionId = Collections.insert({
+      name: 'my colelction',
+      parameters: [
+        'source1',
+        'source2',
+        'source3'
+      ],
+      notes: 'lol',
+      createdAt: new Date()
+    })
+
+    for (var i = 1; i <= 15; i++) {
+      Jobs.insert({
+        name: `Job #${i}`,
+        description: `Description for #${i}`,
+        template: {
+          _id: tplId,
+          name: 'My template',
+          libraries: [
+            'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.2/lodash.min.js'
+          ],
+          code: 'setInterval(function() {result(\'lodash\', {reqNewJob:false} );}, 300)',
+          createdAt: new Date(),
+        },
+        collection: {
+          _id: collectionId,
+          name: 'my colelction',
+          parameters: [
+            'source1',
+            'source2',
+            'source3'
+          ],
+          notes: 'lol',
+          createdAt: new Date()
+        },
+        history: [{
           at: new Date(),
-          userId: userId
-        }
-      ]
-    })*/
+          status: 'created',
+          userId: 'w4foexAJ5YAxAtLT7'
+        }],
+        status: 'stopped',
+        createdAt: new Date()
+      });
+    }
 
   }
 
   if (Meteor.settings.public.demoMode) {
     Accounts.config({
-      forbidClientAccountCreation : true
+      forbidClientAccountCreation: true
     })
   }
 
