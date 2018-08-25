e9ecef864fb6ff6e0b870d1a4fad8a58a8795597
Jose Constela <jose@localhost.localdomain>
2018-08-25 18:35:10 +0000

    Update UI for large amount of jobs, data sets and templates

--- client/modules/functions/router.js
+++ client/modules/functions/router.js
@@ -1,4 +1,58 @@
 
+Router.route('/functions/templates', function () {
+  this.render('templates')
+}, {
+  subscriptions: function() {
+    return [
+      Meteor.subscribe('Templates', {}, {})
+    ];
+  },
+  data: function() {
+    if (this.ready) {
+      return {
+        templates: Templates.find({}, {sort: {createdAt: -1}})
+      }
+    }
+  },
+  name: 'templates.index',
+  title: 'Templates'
+})
+
+Router.route('/functions/templates/add', function () {
+  this.layout('fullWithAppLayout')
+  this.render('templatesAdd')
+}, {
+  name: 'templates.add',
+  title: 'Add',
+  parent: 'templates.index'
+})
+
+Router.route('/functions/templates/:_id/edit', function () {
+  this.layout('fullWithAppLayout')
+  this.render('templatesEdit')
+}, {
+  subscriptions: function() {
+    return [
+      Meteor.subscribe('Templates', {
+        _id: this.params._id
+      }, {})
+    ];
+  },
+  data: function() {
+    if (this.ready) {
+      return {
+        template: Templates.findOne({_id: this.params._id})
+      }
+    }
+  },
+  name: 'templates.edit',
+  title: function() {
+    return (!!this.data() && !!this.data().template) ? this.data().template.name : ''
+  },
+  parent: 'templates.index'
+})
+
+
 Router.route('/functions', function () {
   this.render('functions')
 }, {
@@ -51,3 +105,4 @@ Router.route('/functions/:_id/edit', function () {
   },
   parent: 'functions.index'
 })
+
